505160d0b0e2c61d27817a964030845faf7bb130
cedoradog <cedoradog@unal.edu.co>
2014-07-20 16:57:00 -0500

    Add top_ten.py.

---
+++ assignment1/top_ten.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import json
+import sys
+import string
+from collections import Counter
+
+def has_text(py_line):
+    answer = False
+    if 'text' in py_line.keys():
+        answer = True
+    return answer
+
+def has_hashtags(py_line):
+    answer = False
+    if 'entities' in py_line.keys():
+        if type(py_line['entities']) is dict:
+            if 'hashtags' in py_line['entities'].keys():
+                if py_line['entities']['hashtags'] != []:
+                    answer = True
+    return answer
+
+def main():
+    #Open the downloaded file with tweets in json format
+    tweet_file = open(sys.argv[1], "r")
+    #Initialize a counter for the hashtags
+    hashtag_counter = Counter()
+    count=0
+    #For every tweet in json format
+    for line in tweet_file:
+        #Format as python dictionary
+        py_line = json.loads(line)
+        #If the line has any hashtag
+        if has_hashtags(py_line):
+            count +=1
+            hashtags = py_line['entities']['hashtags']
+            #Encode the hashtags in a list with readable characters
+            nice_hashtags = [hashtag['text'].encode('utf-8').lower() for hashtag in hashtags]
+            #Add the counting of the list to the hashtag_counter
+            hashtag_counter += Counter(nice_hashtags)
+    for (hashtag, frequency) in hashtag_counter.most_common(10):
+        print(hashtag + " " + str(frequency))
+    #Close tweet_file
+    print ('users... '+ str(count))
+    print ('ht... ' + str(len(hashtag_counter)))
+#    print hashtag_counter
+    tweet_file.close()
+    
+
+if __name__ == '__main__':
+    main()
