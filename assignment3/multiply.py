c28fcb9600ed0ba38b2ecf81226766cac237936a
cedoradog <cedoradog@unal.edu.co>
2014-07-30 15:05:51 -0500

    Create multiply.py

---
+++ assignment3/multiply.py
@@ -0,0 +1,66 @@
+import MapReduce
+import sys
+import re
+
+"""
+Compute matrix multiplication.
+Given two sparse matrices represented as a set of records
+(matrix, i, j, val) return its product matrix as a set of
+records (i, j, val).
+"""
+
+mr = MapReduce.MapReduce()
+
+# =============================
+# Do not modify above this line
+
+nrowsA, ncolsA = 5, 5 
+nrowsB, ncolsB = 5, 5
+dimA = (nrowsA, ncolsA)
+dimB = (nrowsB, ncolsB)
+dimAB = (nrowsA, ncolsB)
+
+def mapper(record):
+    """
+    For each record return a set of pairs (key, info) where the keys
+    indicates the (i, j) places where the value will be used in AB 
+    product and the info gives the information of the value itself.
+    """
+    # key: name of matrix and position
+    # value: value
+    matrix = record[0]
+    row, col  = record[1], record[2]
+    value = record[3]
+    if matrix == "a":
+        for colB in range(ncolsB):
+            key = (row, colB)
+            info = ("a", col, value)
+            mr.emit_intermediate(key, info)
+    elif matrix == "b":
+        for rowA in range(nrowsA):
+            key = (rowA, col)
+            info = ("b", row, value)
+            mr.emit_intermediate(key, info)
+
+def reducer(key, infos):
+    """
+    Group 
+    """
+    # key: 
+    # value:
+    row, col = key[0], key[1]
+    for info in infos:
+        value = sum([infoA[2] * infoB[2] for infoA in infos 
+                     for infoB in infos
+                     if (infoA[0] == "a" and 
+                         infoB[0] == "b" and
+                         infoA[1] == infoB[1])])
+
+    result = [row, col, value]
+    mr.emit(result)
+
+# Do not modify below this line
+# =============================
+if __name__ == '__main__':
+  inputdata = open(sys.argv[1])
+  mr.execute(inputdata, mapper, reducer)
